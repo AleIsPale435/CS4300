fb12aa42681cd7c3ae51aa21721a9edd1cafd43d
abarrag4 <alebarr564@gmail.com>
2025-03-03 08:41:04 +0000

    Added Serializers

--- homework2/movie_theater_booking/bookings/models.py
+++ homework2/movie_theater_booking/bookings/models.py
@@ -1,19 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Movie(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
     description = models.TextField()
     releaseDate = models.DateField()
-    duration = models.IntegerField()
+    duration = models.IntegerField()  
+
+    def __str__(self):
+        return self.title
 
 class Seat(models.Model):
-    seatNumber = models.CharField(max_length=255)
+    seatNumber = models.CharField(max_length=10)
     isBooked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.seat_number
 
 class Booking(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    bookingData = models.DateTimeField(auto_now_add=True)
+    bookingDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.movie.title} - {self.seat.seat_number}"
--- homework2/movie_theater_booking/bookings/views.py
+++ homework2/movie_theater_booking/bookings/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import Movie, Seat, Booking
+from .serializers import MovieSerializer, SeatSerializer, BookingSerializer
 
-# Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
+def index(request):
+    return HttpResponse("Hello, world. You're at the booking index.")
